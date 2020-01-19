d5b9ec27dcaaaeef820d5c821f837f4a70374839
Jinesh Mehta <78670+jinesh@users.noreply.github.com>
2020-01-20 02:37:17 +0530

    Update build.js

--- build.js
+++ build.js
@@ -1,5 +1,4 @@
 const metalsmith = require('metalsmith');
-const flatten = require('metalsmith-flatten');
 const drafts = require('metalsmith-drafts');
 const markdown = require('metalsmith-markdown');
 const collections = require('metalsmith-collections');
@@ -37,7 +36,6 @@ metalsmith(__dirname)
     .source('./src')
     .destination('./dist')
     .clean(true)
-    .use(flatten("src"))
     .use(drafts())
     .use(collections({
         notes: {
